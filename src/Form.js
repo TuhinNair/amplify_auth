f53385fde470b8877536c2c0741f9f61cb34fc45
Tuhin Nair <tuhinnair444@gmail.com>
2020-08-16 18:58:54 +0700

    expand renderForm

--- src/Form.js
+++ src/Form.js
@@ -6,6 +6,55 @@ import ConfirmSignUp from './ConfirmSignUp'
 import ForgotPassword from './ForgotPassword'
 import ForgotPasswordSubmit from './ForgotPasswordSubmit'
 
+async function signIn({username, password}, setUser) {
+    try {
+        const user = await Auth.signIn(username, password)
+        const userInfo = {username: user.username, ...user.attributes}
+        setUser(userInfo)
+    } catch (err) {
+        console.log('error signing in: ', err)
+    }
+}
+
+async function signUp({username, password, email}, updateFormType) {
+    try {
+        await Auth.signUp({
+            username, password, attributes: {email}
+        });
+        console.log('Sign up success')
+        updateFormType('confirmSignUp')
+    } catch (err) {
+        console.log('error signing up: ', err)
+    }
+}
+
+async function confirmSignUp({username, confirmationCode}, updateFormType) {
+    try {
+        await Auth.confirmSignUp(username, confirmationCode)
+        updateFormType('signIn')
+    } catch (err) {
+        console.log('error signing up: ', err)
+    }
+}
+
+async function ForgotPassword({username}, updateFormType) {
+    try {
+        await Auth.forgotPassword(username)
+        updateFormType('forgotPasswordSubmit')
+    } catch (err) {
+        console.log('error submitting username to reset password: ', err)
+    }
+}
+
+async function forgotPasswordSubmit({username, confirmationCode, password}, updateFormType) {
+    try {
+        await Auth.forgotPasswordSubmit(username, confirmationCode, password)
+        updateFormType('signIn')
+    } catch (err) {
+        console.log('error updating password: ', err)
+    }
+}
+
 const initialFormState = {
     username: '', password: '', email: '', confirmationCode: ''
 }
@@ -13,7 +62,54 @@ const initialFormState = {
 function Form(props) {
     const [formType, updateFormType] = useState('signIn')
     const [formState, updateFormState] = useState(initialFormState)
-    function renderForm() {}
+
+    function updateForm(event) {
+        const newFormState = {
+            ...formState, [event.target.name]: event.target.value
+        }
+        updateFormState(newFormState)
+    }
+    function renderForm() {
+        switch(formType) {
+            case 'signUp':
+                return (<SignUp
+                    signUp={() => signUp(formState, updateFormType)}
+                    updateFormState={e => updateForm(e)}
+                />)
+            case 'confirmSignUp':
+                return (
+                    <ConfirmSignUp 
+                        confirmSignUp={() => confirmSignUp(formState, updateFormType)}
+                        updateFormState={e => updateForm(e)}
+                    />
+                )
+            case 'signIn':
+                return (
+                    <SignIn
+                        signIn={() => signIn(formState, props.setUser)}
+                        updateFormState={e => updateForm(e)}
+                    />
+                )
+            case 'forgorPassword':
+                return (
+                    <ForgotPassword
+                        forgotPassword={() => forgotPassword(formState, updateFormType)}
+                        updateFormState={e => updateForm(e)}
+                    />
+                )
+            case 'forgorPasswordSubmit':
+                return (
+                    <ForgotPasswordSubmit
+                        forgotPasswordSubmit={() => forgotPasswordSubmit(formState, updateFormType)}
+                        updateFormState={e => updateForm(e)}
+                    />
+                )
+            default:
+                return null
+
+        }
+    }
+
     return (
         <div>
             {renderForm()}
