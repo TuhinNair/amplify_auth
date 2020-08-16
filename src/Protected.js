acf2867c96b83c41b395ca43b0f6922533be58fd
Tuhin Nair <tuhinnair444@gmail.com>
2020-08-16 17:21:47 +0700

    add protected route

---
+++ src/Protected.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+import {Auth} from 'aws-amplify'
+import Container from './Container'
+import protectedRoute from './protectedRoute'
+
+function Protected() {
+    return (
+        <Container>
+            <h1>Protected route</h1>
+        </Container>
+    );
+}
+
+export default protectedRoute(Protected)
